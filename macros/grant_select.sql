2cae3d85780b9d4270473921d0caca77a3d8ecc1
Diana Gromakovskaia <dianagr1996@gmail.com>
2024-12-12 13:12:15 +0000

    added grant_select macro for BQ

---
+++ macros/grant_select.sql
@@ -0,0 +1,10 @@
+{% macro grant_select(project='dbt-project', schema=target.dataset, user='user:dbt-user@your-project.iam.gserviceaccount.com') %}
+    {% set sql %} 
+        grant `roles/bigquery.dataViewer` on schema `{{ project }}.{{ schema }}` to "{{ user }}";
+    {% endset %}
+
+    {{ log('Granting select on all tables and views in schema ' ~ target.dataset ~ ' to user ' ~ user, info=True) }}
+    {% do run_query(sql) %}
+    {{ log('Privileges granted', info=True) }}
+
+{% endmacro %}
